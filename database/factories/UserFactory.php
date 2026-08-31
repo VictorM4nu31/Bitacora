293d434b4f66acb08133da5f7a15b2b9035df8d3
VictorM4nu31 <angelesvictor690@gmail.com>
2026-08-30 18:58:30 -0600

    feat: sembrar empresa demo y usuarios con rol

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -29,11 +31,42 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => UserRole::Technician,
             'remember_token' => Str::random(10),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
         ];
+    }
+
+    /**
+     * Indicate that the user belongs to the given company.
+     */
+    public function forCompany(Company|int $company): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'company_id' => $company instanceof Company ? $company->getKey() : $company,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is an administrator.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::Admin,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is a technician.
+     */
+    public function technician(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::Technician,
+        ]);
     }
 
     /**
