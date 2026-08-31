3193ea2d5bab663c0038dda1b1a377236b3704fc
VictorM4nu31 <angelesvictor690@gmail.com>
2026-08-30 18:58:30 -0600

    feat: agregar modelo Company y enum de roles

---
+++ app/Models/Company.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\CompanyFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string $timezone
+ */
+#[Fillable(['name', 'slug', 'timezone'])]
+class Company extends Model
+{
+    /** @use HasFactory<CompanyFactory> */
+    use HasFactory;
+
+    /**
+     * Users belonging to the company.
+     *
+     * @return HasMany<User, $this>
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+}
